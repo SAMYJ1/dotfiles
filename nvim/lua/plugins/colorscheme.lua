9b2d6541aaa55508d7f684bd1796ab80bf66ff68
马一建 <mayijian@mayijiandeMac-mini.local>
2024-12-29 00:01:32 +0800

    upgrade lazy config & switch theme to catppuccin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,47 @@
 return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = {
+  --     style = "moon",
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --     on_highlights = function(hl, colors)
+  --       hl.LineNrAbove = {
+  --         fg = colors.dark3,
+  --       }
+  --       hl.LineNrBelow = {
+  --         fg = colors.dark3,
+  --       }
+  --     end,
+  --   },
+  -- },
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
     opts = {
-      style = "moon",
-      -- transparent = true,
-      on_highlights = function(hl, colors)
-        hl.LineNrAbove = {
-          fg = colors.dark3,
-        }
-        hl.LineNrBelow = {
-          fg = colors.dark3,
-        }
-      end,
+      transparent_background = true,
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
