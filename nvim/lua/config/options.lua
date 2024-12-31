d59730f9bcc50456caebdfd2af5fbff9696b0013
mayijian <mayijian@meituan.com>
2024-12-31 14:45:51 +0800

    disable system yank default

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = ""
 
 -- vim.g.neovide_window_blurred = true
 -- vim.g.neovide_transparency = 0.9
