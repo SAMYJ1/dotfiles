94a3a1028a001c88639006be63e4bf330d980747
eugene <eugene@x.com>
2025-03-10 18:48:31 +0800

    update config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,5 +4,6 @@
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("i", "<C-p>", "<C-r>0", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-u>", "<esc><C-u>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-d>", "<esc><C-d>", { noremap = true, silent = true })
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,10 +1,11 @@
 return {
-  "snacks.nvim",
-  opts = {
-    scroll = { enabled = false },
-    dashboard = {
-      preset = {
-        header = [[
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+      dashboard = {
+        preset = {
+          header = [[
 ██╗   ██╗ ███████╗  ██████╗  ██████╗  ██████╗  ███████╗
 ██║   ██║ ██╔════╝ ██╔════╝ ██╔═══██╗ ██╔══██╗ ██╔════╝
 ██║   ██║ ███████╗ ██║      ██║   ██║ ██║  ██║ █████╗  
@@ -24,24 +25,65 @@ return {
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
+        },
+      },
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<c-l>"] = { "loclist", mode = { "i", "n" } },
+              ["<Esc>"] = { "close", mode = { "n", "i" } },
+              ["<c-/>"] = { "toggle_help_input", mode = { "n", "i" } },
+            },
+          },
+          list = {
+            keys = {
+              ["<c-l>"] = { "loclist", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          local bufname = vim.api.nvim_buf_get_name(0) -- Get the current buffer's name
+          local scratch_dir = vim.fn.stdpath("data") .. "/scratch" -- Default scratch directory
+
+          Snacks.bufdelete()
+          if bufname and bufname:sub(1, #scratch_dir) == scratch_dir then
+            -- Buffer is a saved scratch buffer
+            os.remove(bufname) -- Delete the file from disk
+            print("Deleted scratch buffer and file: " .. bufname)
+          end
+        end,
+        desc = "Delete (Scratch) Buffer",
       },
     },
   },
-  keys = {
-    {
-      "<leader>bd",
-      function()
-        local bufname = vim.api.nvim_buf_get_name(0) -- Get the current buffer's name
-        local scratch_dir = vim.fn.stdpath("data") .. "/scratch" -- Default scratch directory
-
-        Snacks.bufdelete()
-        if bufname and bufname:sub(1, #scratch_dir) == scratch_dir then
-          -- Buffer is a saved scratch buffer
-          os.remove(bufname) -- Delete the file from disk
-          print("Deleted scratch buffer and file: " .. bufname)
-        end
+  {
+    "folke/trouble.nvim",
+    optional = true,
+    specs = {
+      "folke/snacks.nvim",
+      opts = function(_, opts)
+        return vim.tbl_deep_extend("force", opts or {}, {
+          picker = {
+            actions = require("trouble.sources.snacks").actions,
+            win = {
+              input = {
+                keys = {
+                  ["<c-t>"] = {
+                    "trouble_open",
+                    mode = { "n", "i" },
+                  },
+                },
+              },
+            },
+          },
+        })
       end,
-      desc = "Delete (Scratch) Buffer",
     },
   },
 }
